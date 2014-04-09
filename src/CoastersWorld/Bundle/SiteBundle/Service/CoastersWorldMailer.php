406922461cb30b8aee6f57524ac92a3a57e47d23
Rémi Parent <remi.parent@supinfo.com>
2014-04-09 16:12:47 +0200

    Passage de différentes strings en paramètres d'application : - adresse email admin - nom de l'admin lors de l'envoi de message - sujet du mail lors de l'envoi de l'email d'activation

--- src/CoastersWorld/Bundle/SiteBundle/Service/CoastersWorldMailer.php
+++ src/CoastersWorld/Bundle/SiteBundle/Service/CoastersWorldMailer.php
@@ -7,18 +7,23 @@ use Symfony\Component\Templating\EngineInterface;
 class CoastersWorldMailer
 {
 	protected $mailer;
+	protected $templating;
+	protected $admin_email_address;
+	protected $admin_email_name;
 
-	public function __construct($mailer, $templating)
+	public function __construct($mailer, $templating, $admin_email_address, $admin_email_name)
 	{
 		$this->mailer = $mailer;
 		$this->templating = $templating;
+		$this->admin_email_address = $admin_email_address;
+		$this->admin_email_name = $admin_email_name;
 	}
 
-	public function sendActivationEmail($user)
+	public function sendActivationEmail($user, $subject)
 	{
 		$this->mailer
-			 ->send(\Swift_Message::newInstance()->setSubject('Activation de votre compte CoastersWorld.fr')
-			             						 ->setFrom(array('admin@coastersworld.fr' => "CoastersWorld.fr"))
+			 ->send(\Swift_Message::newInstance()->setSubject($subject)
+			             						 ->setFrom(array($this->admin_email_address => $this->admin_email_name))
 			             						 ->setTo($user->getEmail())
 			             						 ->setBody($this->templating
 			             									    ->render('CoastersWorldSiteBundle:Security:activationemail.txt.twig', 
