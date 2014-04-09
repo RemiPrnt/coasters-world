c465550c5d50cd7d5ab3171fe523359f87f7c8c4
Rémi Parent <remi.parent@supinfo.com>
2014-04-09 15:35:03 +0200

    Indentation Mailer :-)

--- src/CoastersWorld/Bundle/SiteBundle/Service/CoastersWorldMailer.php
+++ src/CoastersWorld/Bundle/SiteBundle/Service/CoastersWorldMailer.php
@@ -16,12 +16,13 @@ class CoastersWorldMailer
 
 	public function sendActivationEmail($user)
 	{
-		$this->mailer->send(\Swift_Message::newInstance()
-            ->setSubject('Activation de votre compte CoastersWorld.fr')
-            ->setFrom(array('admin@coastersworld.fr' => "CoastersWorld.fr"))
-            ->setTo($user->getEmail())
-            ->setBody($this->templating->render('CoastersWorldSiteBundle:Security:activationemail.txt.twig', array(
-                'user' => $user
-        ))));
+		$this->mailer
+			 ->send(\Swift_Message::newInstance()->setSubject('Activation de votre compte CoastersWorld.fr')
+			             						 ->setFrom(array('admin@coastersworld.fr' => "CoastersWorld.fr"))
+			             						 ->setTo($user->getEmail())
+			             						 ->setBody($this->templating
+			             									    ->render('CoastersWorldSiteBundle:Security:activationemail.txt.twig', 
+			             									    	array('user' => $user)))
+        	 );
 	}
 }
