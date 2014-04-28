f80e9d0f37444da6833c0fa5c18f1b835cdc23c6
Rémi Parent <remi.parent@supinfo.com>
2014-04-28 20:14:26 +0200

    Revue de code sur SecurityController

--- src/CoastersWorld/Bundle/SiteBundle/Controller/SecurityController.php
+++ src/CoastersWorld/Bundle/SiteBundle/Controller/SecurityController.php
@@ -13,6 +13,10 @@ use CoastersWorld\Bundle\SiteBundle\Form\Type\ChangePasswordType;
 
 class SecurityController extends Controller
 {
+    /**
+     * loginAction
+     * Affiche le formulaire de connexion
+     */
     public function loginAction()
     {
         $request = $this->getRequest();
@@ -33,22 +37,26 @@ class SecurityController extends Controller
             array(
                 // last username entered by the user
                 'last_username' => $session->get(SecurityContext::LAST_USERNAME),
-                'error'         => $error,
+                'error'         => $error
             )
         );
     }
 
+    /**
+     * registerAction
+     * Affiche le formulaire d'inscription, vérifie les informations et enregistre le nouveau membre
+     */
     public function registerAction()
     {
-        $em = $this->getDoctrine()->getManager();
+        $em         = $this->getDoctrine()->getManager();
+        $request    = $this->getRequest();
         
         $user = new User();
         $form = $this->createForm(new UserType, $user);
 
-        $request = $this->get('request');
         if("POST" === $request->getMethod())
         {
-            $form->handleRequest($this->getRequest());
+            $form->handleRequest($request);
 
             $users_repository = $em->getRepository('CoastersWorldSiteBundle:User');
             $error = false;
@@ -57,27 +65,31 @@ class SecurityController extends Controller
             {
                 $error = true;
                 $form->get('username')->addError(
-                    new FormError('Ce pseudo est déjà utilisé. Veuillez en choisir un autre.'));
+                    new FormError('Ce pseudo est déjà utilisé. Veuillez en choisir un autre.')
+                );
             }
             
             if($users_repository->findBy(array('email' => $user->getEmail())))
             {
                 $error = true;
                 $form->get('email')->addError(
-                    new FormError('Cette adresse e-mail est déjà liée à un autre compte. Veuillez en choisir une autre.'));
+                    new FormError('Cette adresse e-mail est déjà liée à un autre compte. Veuillez en choisir une autre.')
+                );
             }
 
             if(!$error && $form->isValid())
             {
-                $user->setPassword($this->container
-                    ->get('security.encoder_factory')
-                    ->getEncoder($user)
-                    ->encodePassword($user->getPassword(), $user->getSalt()));
+                $user->setPassword(
+                    $this->container
+                         ->get('security.encoder_factory')
+                         ->getEncoder($user)
+                         ->encodePassword($user->getPassword(), $user->getSalt())
+                );
                 $em->persist($user);
                 $em->flush();
 
                 // Création du message de succès de l'inscription
-                $flashbag = $this->get("session")->getFlashBag();
+                $flashbag = $request->getSession()->getFlashBag();
                 $flashbag->add('register_succeed_username', $user->getUsername());
                 $flashbag->add('register_succeed_email',    $user->getEmail());
 
@@ -93,13 +105,20 @@ class SecurityController extends Controller
         ));
     }
 
+    /**
+     * registerSucceedAction
+     * Affiche un message de succès si l'inscription a été prise en compte
+     */
     public function registerSucceedAction()
     {
-        $session = $this->get("session");
+        $session = $this->getRequest()->getSession();
 
+        // L'utilisateur ne vient pas de s'enregister et tente d'accéder à la page
         if( !$session->getFlashBag()->has('register_succeed_username') &&
-            !$session->getFlashBag()->has('register_succeed_email') ) // L'utilisateur ne vient pas de s'enregister et tente d'accéder à la page
-            return $this->redirect($this->generateUrl('coasters_world_homepage'));
+            !$session->getFlashBag()->has('register_succeed_email') )
+            return $this->redirect(
+                $this->generateUrl('coasters_world_homepage')
+            );
 
         return $this->render('CoastersWorldSiteBundle:Security:registersucceed.html.twig', array(
             'register_succeed_username' => $session->getFlashBag()->get('register_succeed_username')[0],
@@ -107,6 +126,10 @@ class SecurityController extends Controller
         ));
     }
 
+    /**
+     * activateAction
+     * Active un compte utilisateur via le lien envoyé par e-mail lors de l'inscription
+     */
     public function activateAction($userid,$key)
     {
         $em = $this->getDoctrine()->getManager();
@@ -136,22 +159,33 @@ class SecurityController extends Controller
         ));
     }
 
+    /**
+     * activateSucceedAction
+     * Affiche un message de succès si l'activation a été prise en compte
+     */
     public function activateSucceedAction()
     {
-        $session = $this->get("session");
+        $session = $this->getRequest()->getSession();
 
-        if(!$session->getFlashBag()->has('activate_succeed_username')) // L'utilisateur ne vient pas d'activer son compte   et tente d'accéder à la page
-            return $this->redirect($this->generateUrl('coasters_world_homepage'));
+         // L'utilisateur ne vient pas de s'enregister et tente d'accéder à la page
+        if(!$session->getFlashBag()->has('activate_succeed_username'))
+            return $this->redirect(
+                $this->generateUrl('coasters_world_homepage')
+            );
 
         return $this->render('CoastersWorldSiteBundle:Security:activatesucceed.html.twig', array(
             'activate_succeed_username' => $session->getFlashBag()->get('activate_succeed_username')[0]
         ));
     }
 
+    /**
+     * motdepasseOublieAction
+     * Affiche un formulaire permettant d'inscrire son adresse e-mail pour récupérer son mot de passe
+     */
     public function motdepasseOublieAction()
     {
-        $session = $this->get('session');
-        $request = $this->get('request');
+        $session = $this->getRequest()->getSession();
+        $request = $this->getRequest();
 
         // L'email de changement de mot de passe vient d'être envoyé, affichage d'un message de confirmation
         if($session->getFlashBag()->has('password_reset_email'))
@@ -189,11 +223,14 @@ class SecurityController extends Controller
                 // Envoi de l'email contenant le lien vers le formulaire de changement de mot de passe
                 $this->get('coasters_world.mailer')->sendPasswordResetEmail($user);
                 
-                return $this->redirect($this->generateUrl('coasters_world_mot_de_passe_oublie'));
+                return $this->redirect(
+                    $this->generateUrl('coasters_world_mot_de_passe_oublie')
+                );
             }
 
             $form->get('email')->addError(
-                    new FormError('Cette adresse e-mail n\'est rattachée à aucun compte utilisateur.'));
+                new FormError('Cette adresse e-mail n\'est rattachée à aucun compte utilisateur.')
+            );
         }
 
         return $this->render('CoastersWorldSiteBundle:Security:motdepasseoublie.html.twig', array(
@@ -201,6 +238,10 @@ class SecurityController extends Controller
         ));
     }
 
+    /**
+     * changePasswordAction
+     * Affiche un message de succès si la modification du mot de passe a été prise en compte
+     */
     public function changePasswordAction($userid,$key)
     {
         $em = $this->getDoctrine()->getManager();
@@ -209,7 +250,9 @@ class SecurityController extends Controller
         $user = $users_repository->find($userid);
 
         if(is_null($user))
-            return $this->redirect($this->generateUrl('coasters_world_homepage'));
+            return $this->redirect(
+                $this->generateUrl('coasters_world_homepage')
+            );
 
         if($key != $user->getChangePasswordKey())
             return $this->render('CoastersWorldSiteBundle:Security:changepassword.html.twig', array(
@@ -223,10 +266,10 @@ class SecurityController extends Controller
 
         $form = $this->createForm(new ChangePasswordType, $user);
 
-        $request = $this->get('request');
+        $request = $this->getRequest();
         if("POST" === $request->getMethod())
         {
-            $form->handleRequest($this->getRequest());
+            $form->handleRequest($request);
 
             $user->setPassword($this->container
                  ->get('security.encoder_factory')
@@ -238,10 +281,12 @@ class SecurityController extends Controller
             $em->flush();
 
             // Création du message de succès de l'activation
-            $flashbag = $this->get("session")->getFlashBag();
+            $flashbag = $this->getRequest()->getSession()->getFlashBag();
             $flashbag->add('change_password_succeed_username', $user->getUsername());
 
-            return $this->redirect($this->generateUrl('coasters_world_change_password_succeed'));
+            return $this->redirect(
+                $this->generateUrl('coasters_world_change_password_succeed')
+            );
         }
 
         return $this->render('CoastersWorldSiteBundle:Security:changepassword.html.twig', array(
@@ -249,13 +294,19 @@ class SecurityController extends Controller
         ));
     }
 
+    /**
+     * changePasswordAction
+     * Affiche un formulaire permettant de modifier son mot de passe
+     */
     public function changePasswordSucceedAction()
     {
-        $session = $this->get("session");
+        $session = $this->getRequest()->getSession();
 
         // L'utilisateur ne vient pas de modifier sont mot de passe et tente d'accéder à la page
         if(!$session->getFlashBag()->has('change_password_succeed_username')) 
-            return $this->redirect($this->generateUrl('coasters_world_homepage'));
+            return $this->redirect(
+                $this->generateUrl('coasters_world_homepage')
+            );
 
         return $this->render('CoastersWorldSiteBundle:Security:changepasswordsucceed.html.twig', array(
             'change_password_succeed_username' => $session->getFlashBag()->get('change_password_succeed_username')[0]
