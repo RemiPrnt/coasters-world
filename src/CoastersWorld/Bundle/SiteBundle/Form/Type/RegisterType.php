dccd284d242a0d1636a92d80d2323f110ca126e1
Florian Blond <florian.blond@gmail.com>
2014-05-31 16:34:53 +0200

    mise en forme inscription

--- src/CoastersWorld/Bundle/SiteBundle/Form/Type/RegisterType.php
+++ src/CoastersWorld/Bundle/SiteBundle/Form/Type/RegisterType.php
@@ -8,7 +8,6 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class RegisterType extends AbstractType
 {
-
     private $translator;
 
     public function __construct($translator)
@@ -22,26 +21,41 @@ class RegisterType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-
-        $builder->add('username',   'text',     array(  'label' => "register.form.pseudo.label",
-                                                        'attr' => array(    'rel' => "tooltip",
-                                                                            'data-original-title' => $this->translator->trans("register.form.pseudo.tooltip"))))
-                ->add('password',   'repeated', array(
-                                                    'type' => 'password',
-                                                    'invalid_message' => 'Les mots de passe doivent correspondre',
-                                                    'options' => array('required' => true),
-                                                    'first_options'  => array(  'label' => 'register.form.password.label',
-                                                                                'attr' => array('rel' => "tooltip",
-                                                                                                'data-original-title' => $this->translator->trans("register.form.password.tooltip"))),
-                                                    'second_options' => array(  'label' => 'register.form.passwordConfirm.label',
-                                                                                'attr' => array('rel' => "tooltip",
-                                                                                                'data-original-title' => $this->translator->trans("register.form.passwordConfirm.tooltip")))))
-                ->add('email',      'email',    array(  'label' => "register.form.email.label",
-                                                        'attr' => array('rel' => "tooltip",
-                                                                        'data-original-title' => $this->translator->trans("register.form.email.tooltip"))))
+        $builder
+            ->add('username', 'text', array(
+                'label' => "register.form.pseudo.label",
+                'attr' => array(
+                    'rel' => "tooltip",
+                    'data-original-title' => $this->translator->trans("register.form.pseudo.tooltip"),
+                    'placeholder' => 'Pseudo'
+                )
+            ))
+            ->add('email', 'email', array(
+                'label' => "register.form.email.label",
+                'attr' => array(
+                    'rel' => "tooltip",
+                    'data-original-title' => $this->translator->trans("register.form.email.tooltip"))
+            ))
+            ->add('password', 'repeated', array(
+                'type' => 'password',
+                'invalid_message' => 'Les mots de passe doivent correspondre',
+                'options' => array('required' => true),
+                'first_options'  => array(
+                    'label' => 'register.form.password.label',
+                    'attr' => array(
+                        'rel' => "tooltip",
+                        'data-original-title' => $this->translator->trans("register.form.password.tooltip")
+                )),
+                'second_options' => array(
+                    'label' => 'register.form.passwordConfirm.label',
+                    'attr' => array(
+                        'rel' => "tooltip",
+                        'data-original-title' => $this->translator->trans("register.form.passwordConfirm.tooltip")
+                ))
+            ))
         ;
     }
-    
+
     /**
      * @param OptionsResolverInterface $resolver
      */
@@ -57,6 +71,6 @@ class RegisterType extends AbstractType
      */
     public function getName()
     {
-        return 'coastersworld_bundle_sitebundle_user';
+        return 'cw_user';
     }
 }
